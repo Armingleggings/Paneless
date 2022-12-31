00129a710fa00affefced2d5dd54ebbbf1a01c49
Jeremy Duffy <jeremyduffy@microsoft.com>
2022-12-31 12:33:26 -0800

    lasty

--- MyWay2/obj/Debug/netcoreapp3.1/App.g.cs
+++ MyWay2/obj/Debug/netcoreapp3.1/App.g.cs
@@ -45,7 +45,7 @@ namespace Paneless {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Paneless {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
         public static void Main() {
             Paneless.App app = new Paneless.App();
             app.InitializeComponent();
